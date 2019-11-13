1aa9ac69d42e31727bf9cdf3fc62b88b51410847
21102341 <zakharafonin@icloud.com>
2019-11-13 20:07:20 +0300

    Modularity and base decoders

--- main.py
+++ main.py
@@ -2,35 +2,17 @@ import socket
 import struct
 import time
 
+import net_packet
+
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 c = 0
 time_s = time.time()
 
 while True:
-    packet = s.recvfrom(65565)
+    packet = s.recvfrom(65565)[0]
     time_c = time.time()
 
-    packet = packet[0]
-
-    ip_header = struct.unpack('!BBHHHBBH4s4s', packet[0:20])
-
-    magic_verihl = ip_header[0]
-    version = magic_verihl >> 4
-    ihl = magic_verihl & 0xF
-
-    iph_length = ihl * 4
-
-    ttl = ip_header[5]
-    protocol = ip_header[6]
-    addr_s = socket.inet_ntoa(ip_header[8])
-    addr_d = socket.inet_ntoa(ip_header[9])
-
-    print(f'''[#{c}, {time_c - time_s}]
-    Version: {version}
-    IHL: {ihl}
-    TTL: {ttl}
-    Protocol: {protocol}
-    Source Address: {addr_s}
-    Destination Address: {addr_d}''')
-
+    mac_src, mac_dst = net_packet.net_decode(packet)
+    
+    print(f'#{c} - {time_c}\nSource: {mac_src}\nDestination: {mac_dst}\n')
     c += 1
---
+++ net_packet.py
@@ -0,0 +1,21 @@
+import struct
+import socket
+
+
+def eth_addr(raw, boo):
+    if boo:
+        print(raw)
+    toi = lambda a: '{:02x}'.format(raw[a])
+    return f'{toi(0)}:{toi(1)}:{toi(2)}:{toi(3)}:{toi(4)}:{toi(5)}'
+  
+def net_decode(packet):
+    eth_length = 14
+
+    eth_header = packet[0:eth_length]
+    eth = struct.unpack('!6s6sH', eth_header)
+    eth_protocol = socket.ntohs(eth[2])
+
+    mac_src = eth_addr(packet[6:12], True)
+    mac_dst = eth_addr(packet[0:6], False)
+
+    return mac_src, mac_dst
