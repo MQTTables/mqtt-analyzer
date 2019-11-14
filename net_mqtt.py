46e0f90175c75a860c3cc13801aa46c4f7e6406a
somvy <xenon.a@ya.ru>
2019-11-14 12:46:16 +0300

    -'

--- net_mqtt.py
+++ net_mqtt.py
@@ -7,5 +7,5 @@ packets = rdpcap(input())
 for packet in packets:
     print(packet.summary())
     print(packet.show())
-    '''if packet.haslayer(mqtt.MQTT):
-        print('MQTT')'''
+    if packet.haslayer(mqtt.MQTT):
+        print('MQTT')
