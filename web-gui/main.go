4c8c979760ccfdd4ccf48437726c6549fe2557df
sh2nk <nekkt.sh@gmail.com>
2019-11-15 14:07:04 +0300

    split go part

--- web-gui/main.go
+++ web-gui/main.go
@@ -1,57 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"io"
 	"log"
 	"net/http"
-	"os"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseGlob("templates/*.html")
-	if err != nil {
-		log.Fatalf("Template parsing error: %s", err)
-	}
-	tmpl.Execute(w, nil)
-}
-
-func loadAll(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func upload(w http.ResponseWriter, r *http.Request) {
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		fmt.Fprintf(w, "Upload error: %s", err)
-		return
-	}
-	defer file.Close()
-
-	out, err := os.Create(header.Filename)
-	if err != nil {
-		fmt.Fprintf(w, "Unable to create the file for writing. Error: %s", err)
-		return
-	}
-
-	defer out.Close()
-	_, err = io.Copy(out, file)
-	if err != nil {
-		fmt.Fprintf(w, "Server io error: %s", err)
-	}
-	http.Redirect(w, r, "/", 301)
-}
-
+//main - Main function
 func main() {
+	//Mux and handlers
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	mux.HandleFunc("/", index)
 	mux.HandleFunc("/upload", upload)
+	mux.HandleFunc("/loadall", loadAll)
+
+	//Web server configuration
 	serv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 	log.Printf("Started on %s", serv.Addr)
+
+	//Main application thread
 	log.Fatal(serv.ListenAndServe())
 }
